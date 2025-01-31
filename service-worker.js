b116bc798db6e6bf1d54f654fc9ba1339c10b541
Victor Petersson <victor.petersson05123@gmail.com>
2025-01-31 09:48:25 +0100

    manifest

--- service-worker.js
+++ service-worker.js
@@ -1,92 +1,62 @@
-/* ===========================================================
- * docsify sw.js
- * ===========================================================
- * Copyright 2016 @huxpro
- * Licensed under Apache 2.0
- * Register service worker.
- * ========================================================== */
+const staticCacheName = "site-static-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./index.js",
+  "./manifest.json",
+  "./bilder/background.jpg",
+  "./bilder/1.jpg",
+  "./bilder/4.jpg",
+  "./bilder/icon512_maskable.png",
+  "./main.css",
+  "./main.css.map",
+  "./bootstrap.min.css",
+  "./script.js",
+  "./st.html",
+  "./bilder/5.jpg",
+];
 
-const RUNTIME = 'docsify'
-const HOSTNAME_WHITELIST = [
-  self.location.hostname,
-  'fonts.gstatic.com',
-  'fonts.googleapis.com',
-  'cdn.jsdelivr.net'
-]
-const NETWORK_TIMEOUT_MS = 500
+self.addEventListener("install", (evt) => {
+  console.log("service worker has been installed");
+  evt.waitUntil(
+    caches
+      .open(staticCacheName)
+      .then((cache) => {
+        console.log("caching shell assets");
+        return cache.addAll(assets);
+      })
+      .catch((error) => {
+        console.error("Cache addAll failed:", error);
+      })
+  );
+  self.skipWaiting();
+});
 
-// The Util Function to hack URLs of intercepted requests
-const getFixedUrl = (req) => {
-  var now = Date.now()
-  var url = new URL(req.url)
+self.addEventListener("activate", (evt) => {
+  console.log("service worker has been activated");
+  evt.waitUntil(
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then((keys) => {
+        return Promise.all(
+          keys
+            .filter((key) => key !== staticCacheName)
+            .map((key) => caches.delete(key))
+        );
+      }),
+    ])
+  );
+});
 
-  // 1. fixed http URL
-  // Just keep syncing with location.protocol
-  // fetch(httpURL) belongs to active mixed content.
-  // And fetch(httpRequest) is not supported yet.
-  url.protocol = self.location.protocol
-
-  // 2. add query for caching-busting.
-  // Github Pages served with Cache-Control: max-age=600
-  // max-age on mutable content is error-prone, with SW life of bugs can even extend.
-  // Until cache mode of Fetch API landed, we have to workaround cache-busting with query string.
-  // Cache-Control-Bug: https://bugs.chromium.org/p/chromium/issues/detail?id=453190
-  if (url.hostname === self.location.hostname) {
-    url.search += (url.search ? '&' : '?') + 'cache-bust=' + now
-  }
-  return url.href
-}
-
-/**
- *  @Lifecycle Activate
- *  New one activated when old isnt being used.
- *
- *  waitUntil(): activating ====> activated
- */
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim())
-})
-
-/**
- *  @Functional Fetch
- *  All network requests are being intercepted here.
- *
- *  void respondWith(Promise<Response> r)
- */
-self.addEventListener('fetch', event => {
-  // Skip some of cross-origin requests, like those for Google Analytics.
-  if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1) {
-    // Stale-while-revalidate
-    // similar to HTTP's stale-while-revalidate: https://www.mnot.net/blog/2007/12/12/stale
-    // Upgrade from Jake's to Surma's: https://gist.github.com/surma/eb441223daaedf880801ad80006389f1
-    const cached = caches.match(event.request)
-    const fixedUrl = getFixedUrl(event.request)
-    const fetched = fetch(fixedUrl, { cache: 'no-store' })
-    const fetchedCopy = fetched.then(resp => resp.clone())
-
-    // Call respondWith() with whatever we get first.
-    // If the fetch fails (e.g disconnected), wait for the cache.
-    // If there’s nothing in cache, wait for the fetch.
-    // If neither yields a response, return offline pages.
-
-    // Updated 2/7/2023 by @zteutsch. Switched from pure race between cache and network to a half second (500 ms) timeout
-    // before going to cache. All fallbacks should still work and cache will be updated in the background.
-
-    const delayCacheResponse = new Promise((resolve) => {
-      setTimeout(resolve, NETWORK_TIMEOUT_MS, cached);
-    })
-
-    event.respondWith(
-      Promise.race([fetched.catch(_ => cached), delayCacheResponse])
-        .then(resp => resp || fetched)
-        .catch(_ => { /* eat any errors */ })
-    )
-
-    // Update the cache with the version we fetched (only for ok status)
-    event.waitUntil(
-      Promise.all([fetchedCopy, caches.open(RUNTIME)])
-        .then(([response, cache]) => response.ok && cache.put(event.request, response))
-        .catch(_ => { /* eat any errors */ })
-    )
-  }
-})
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches
+      .match(evt.request)
+      .then((cacheRes) => {
+        return cacheRes || fetch(evt.request);
+      })
+      .catch(() => {
+        return fetch(evt.request);
+      })
+  );
+});
